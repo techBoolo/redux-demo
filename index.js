e2802a41cb020f262a1dd3b0ee296d197b2e9f10
techboolo <teklufa@gmail.com>
2021-07-15 14:03:49 +0300

    include unsubscribe and allow users to increase the count with any number N

--- index.js
+++ index.js
@@ -3,6 +3,17 @@ const { createStore } = require('redux');
 // define initial state
 const initialState = { count: 0, more: 'can have more properties' }
 
+// actions are objects with 'type' field
+const inc = { type: 'inc' }
+const dec = { type: 'dec' }
+// actions can also be created with action creator function
+const reset = () => { 
+  return {type: 'reset' }
+}
+const incByN = (N) => {
+  return { type: 'incByN', value: N }
+}
+
 // define a reducer which has two arguments 'state' and 'action'
 const reducer = (state=initialState, action) => {
   switch(action.type) {
@@ -12,35 +23,24 @@ const reducer = (state=initialState, action) => {
       return { ...state, count: state.count - 1 }
     case 'reset':
       return { ...state, count: 0 }
-    case 'incBy5':
+    case 'incByN':
       return { ...state, count: state.count + action.value }
     default:
       return state
   }
 }
 
-// actions are objects with 'type' field
-const inc = { type: 'inc' }
-const dec = { type: 'dec' }
-
-// actions can also be created with action creator function
-const reset = () => { 
-  return {type: 'reset' }
-}
-const incBy5 = () => {
-  return { type: 'incBy5', value: 5 }
-}
-
 // create a store that contains our global state
 const store = createStore(reducer);
 
+// get the state of our initial state of the store
+console.log(store.getState());
+
 // run every subscriber functions when any state changes, can be used to update the ui
-store.subscribe(() => {
+const unsubscribe = store.subscribe(() => {
   console.log(`count is : ${ store.getState().count }`)
 })
 
-// get the state of our store
-console.log(store.getState());
 // dispatch the action to the store
 store.dispatch(inc);
 store.dispatch(inc);
@@ -48,6 +48,11 @@ store.dispatch(inc);
 store.dispatch(dec);
 store.dispatch(reset());
 store.dispatch(dec);
-store.dispatch(incBy5());
+store.dispatch(incByN(8));
 console.log(store.getState());
-
+unsubscribe()
+// after unsubscribing subsequent changes are not logged
+// to log we have to subscribe or use getState()
+store.dispatch(dec);
+store.dispatch(incByN(8));
+console.log(store.getState());
